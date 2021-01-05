9759e1a5b590b51108e9ee3e6120be034b6c0c70
Calexxxxx <pasm28@hotmail.nl>
2021-01-05 13:18:01 +0100

    Update packages

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -3,7 +3,7 @@ module.exports = [{
       options: {"plugins":[],"pathToConfigModule":"src/styles/typography/index.js"},
     },{
       plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"Furry Canines","short_name":"Furry Canines","start_url":"/","background_color":"#fff","theme_color":"#136176","display":"standalone","icon":"./images/favicon.png"},
+      options: {"plugins":[],"name":"Furry Canines","short_name":"Furry Canines","start_url":"/","background_color":"#fff","theme_color":"#136176","display":"standalone","icon":"./images/favicon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"94428d2e2219774d4a4be89536ec4216"},
     },{
       plugin: require('../node_modules/gatsby-plugin-offline/gatsby-browser.js'),
       options: {"plugins":[],"precachePages":["/404","/about","/affiliate-disclosure","/index","/privacy-policy","/dog-breeds","/dog-care","/dog-health","/dog-training","/dog-supplies"]},
--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -3,7 +3,7 @@ var plugins = [{
       options: {"plugins":[]},
     },{
       plugin: require('/Users/pascal/Desktop/furrycanines/node_modules/gatsby-plugin-sitemap/gatsby-ssr'),
-      options: {"plugins":[],"output":"/sitemap.xml"},
+      options: {"plugins":[],"output":"/sitemap.xml","createLinkInHead":true},
     },{
       plugin: require('/Users/pascal/Desktop/furrycanines/node_modules/gatsby-plugin-typography/gatsby-ssr'),
       options: {"plugins":[],"pathToConfigModule":"src/styles/typography/index.js"},
@@ -12,7 +12,7 @@ var plugins = [{
       options: {"plugins":[],"lang":"en"},
     },{
       plugin: require('/Users/pascal/Desktop/furrycanines/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
-      options: {"plugins":[],"name":"Furry Canines","short_name":"Furry Canines","start_url":"/","background_color":"#fff","theme_color":"#136176","display":"standalone","icon":"./images/favicon.png"},
+      options: {"plugins":[],"name":"Furry Canines","short_name":"Furry Canines","start_url":"/","background_color":"#fff","theme_color":"#136176","display":"standalone","icon":"./images/favicon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"94428d2e2219774d4a4be89536ec4216"},
     },{
       plugin: require('/Users/pascal/Desktop/furrycanines/node_modules/gatsby-plugin-offline/gatsby-ssr'),
       options: {"plugins":[],"precachePages":["/404","/about","/affiliate-disclosure","/index","/privacy-policy","/dog-breeds","/dog-care","/dog-health","/dog-training","/dog-supplies"]},
